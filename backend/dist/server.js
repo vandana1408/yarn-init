169b44f146e881fb69f89380d138fc908d1e68af
vandana <vandanaprathapar@gmail.com>
2025-07-12 15:57:30 +1000

    adding dist folder to project in gh

---
+++ backend/dist/server.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/server.ts
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 5000;
+app.get('/', (req, res) => {
+    res.send('Hello from the TypeScript backend! I think?');
+});
+app.listen(port, () => {
+    console.log(`Backend server listening at http://localhost:${port}`);
+});
